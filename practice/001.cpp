7bf1abad0583751d614ddddb80f6bb8add752dc9
luanwushencai <418570398@qq.com>
2021-03-17 20:16:08 +0800

    测试更新

--- practice/001.cpp
+++ practice/001.cpp
@@ -2,14 +2,17 @@
 //test 1111
 int main()
 {
-    int i,j;
-    for(i=0;i<8;i++)
-    {
-        for(j=0;j<8;j++)
-            if((i+j)%2==0)
-                printf("%c%c",219,219);
-            else printf("  ");
-        printf("\n");
+//    int i,j;
+//    for(i=0;i<8;i++)
+//    {
+//        for(j=0;j<8;j++)
+//            if((i+j)%2==0)
+//                printf("%c%c",219,219);
+//            else printf("  ");
+//        printf("\n");
+//    }
+    int v=0;
+    for(v=1;printf("v=%d;",v),printf("v+1=%d;\r\n",v+1), v<7;v++){
     }
     return 0;
 }
